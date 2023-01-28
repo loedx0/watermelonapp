b038c905a9acba713317d69d4e1ef20513ed45b8
Orlando Espinoza <orlando@Orlandos-MacBook-Air.local>
2023-01-27 19:35:58 -0600

    porfin heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Juanjo!')
 });
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 });

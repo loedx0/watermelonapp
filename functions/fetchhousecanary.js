4617acc78d7fed1f384ac018b2e2ed1a4cb5617d
Orlando Espinoza <orlando@Orlandos-MacBook-Air.local>
2023-02-01 16:15:05 -0600

    trying parameters

--- functions/fetchhousecanary.js
+++ functions/fetchhousecanary.js
@@ -9,8 +9,6 @@ module.exports = async function getHouseCanaryData (address, zipcode) {
     try {
         const response = await axios.get(`https://api.housecanary.com/v2/${ENDPOINT}?address=${address}&zipcode=${zipcode}`, {
           auth: {
-            //username:"test_YYI3X9CY6NV23H6DU70O",
-            //password:"ryabhMVxkdq8uFVsYpJ6oMUHzBc5zf0f",
             username: process.env.HC_API_KEY,
             password: process.env.HC_API_SECRET,
           }

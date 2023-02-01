94b96c2150dc5f58c1a8ebced55dbc2d2c4fbc86
Orlando Espinoza <orlando@Orlandos-MacBook-Air.local>
2023-02-01 16:08:35 -0600

    trying parameters

--- api/fetch_house.js
+++ api/fetch_house.js
@@ -4,10 +4,10 @@ const getHouseCanaryData = require ("../functions/fetchhousecanary");
 
 recordRoutes.route("/api/boiler").get(async function (req, res) {
 
-    //const address = req.query.address;
-    //const zipcode = req.query.zipcode;
-    const address = "517 N Chugach St";
-    const zipcode = "99645";
+    const address = req.query.address;
+    const zipcode = req.query.zipcode;
+    //const address = "517 N Chugach St";
+    //const zipcode = "99645";
 
     try {
         const r = await getHouseCanaryData(address, zipcode);

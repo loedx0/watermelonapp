67c45f652f747416d7113c616aa0fc8561d88be5
Orlando Espinoza <orlando@Orlandos-MacBook-Air.local>
2023-02-01 16:01:20 -0600

    trying parameters

--- functions/fetchhousecanary.js
+++ functions/fetchhousecanary.js
@@ -1,16 +1,16 @@
 const axios = require("axios");
 
 const ENDPOINT = "property/value";
-const address = "87 Misty Forest Dr";
-const zipcode = "36869";
+//const address = "87+Misty+Forest+Dr";
+//const zipcode = "36869";
 
 
 module.exports = async function getHouseCanaryData (address, zipcode) {
     try {
-        const response = await axios.get(`https://api.housecanary.com/v2/${ENDPOINT}?$address=${address}&zipcode=${zipcode}`, {
+        const response = await axios.get(`https://api.housecanary.com/v2/${ENDPOINT}?address=${address}&zipcode=${zipcode}`, {
           auth: {
-            username: process.env.HC_API_KEY,
-            password: process.env.HC_API_SECRET,
+            username:"test_YYI3X9CY6NV23H6DU70O",
+            password:"ryabhMVxkdq8uFVsYpJ6oMUHzBc5zf0f",
           }
         });
         console.log(response.data[0]['property/value']['result'])
